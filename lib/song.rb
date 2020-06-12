561bc4cb58b901e7d514fd5f0bf9fc4586f67701
Javi Leyva <jvrleyva@gmail.com>
2020-06-12 20:10:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,3 +14,24 @@ class Song
   #  binding.pry
     new_song.save
   end
+  
+  def self.find_by_artist(artist)
+    Song.all.select do |song|
+      song.artist == artist
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+  end
+
+  def save
+    @@all << self
+    @self
+  end
+
+end
