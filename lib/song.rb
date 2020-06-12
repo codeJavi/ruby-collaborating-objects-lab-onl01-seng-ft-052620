d4f5b755003173f3a3439508524e87272514bd9b
Javi Leyva <jvrleyva@gmail.com>
2020-06-12 20:30:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,14 @@ class Song
   def initialize(name)
     @name = name
   end
+  
+  def save
+    @@all<< self 
+  end 
+  
+  def self.all
+    @@all
+  end 
 
   def self.new_by_filename(file)
     artist_name = file.split(" - ")[0]
