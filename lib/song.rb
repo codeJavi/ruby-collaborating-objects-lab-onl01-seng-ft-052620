77a99dd0af2055ab87ad53143bae5cd415ee20de
Javi Leyva <jvrleyva@gmail.com>
2020-06-12 20:48:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song
   attr_accessor :name, :artist
-  
+
   def initialize(name)
     @name = name
   end
@@ -10,8 +10,8 @@ class Song
     song_name = file.split(" - ")[1]
     song = Song.new(song_name)
     song.artist_name = artist_name
+    song
   end
-
 
   def artist_name=(name)
     artist = Artist.find_or_create_by_name(name)
